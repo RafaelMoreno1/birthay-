6f7b1a0cab2b09bf8ffeebcb44a3b1c6d7fe8cc3
Armando Rafael Moreno Sorrosa <rafaelmoreno1@manchoruco2@hotmail.com>
2020-12-06 23:59:19 -0600

    concxion con firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app"
+const firebaseConfig = {
+    apiKey: "AIzaSyBMOAiTHzM1p6b2TSOB8hW-PyiYRsttaOk",
+    authDomain: "birthay-57e57.firebaseapp.com",
+    databaseURL: "https://birthay-57e57.firebaseio.com",
+    projectId: "birthay-57e57",
+    storageBucket: "birthay-57e57.appspot.com",
+    messagingSenderId: "1093768124494",
+    appId: "1:1093768124494:web:329c03dd58e52fb7dbcb1f",
+    measurementId: "G-M7J9D5K6PC"
+  };
+  // Initialize Firebase
+  export default firebase.initializeApp(firebaseConfig);
